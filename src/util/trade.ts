58afa5fe982ec2eed243e3ca7fadbd398d0d494f
Maxence Raballand <dev@maxencerb.com>
2024-04-02 21:46:54 +0200

    Overrestimate gas

--- src/util/trade.ts
+++ src/util/trade.ts
@@ -553,7 +553,7 @@ class Trade {
     // If not given an explicit gasLimit then we estimate it. Ethers does this automatically, but if we are given a lower bound,
     // (for instance from our own estimateGas function) then we need to invoke estimation manually and compare.
     if (!overrides.gasLimit && gasLowerBound) {
-      overrides.gasLimit = await estimateTx(...args);
+      overrides.gasLimit = (await estimateTx(...args)).mul(120).div(100);
       if (overrides.gasLimit.lt(gasLowerBound)) {
         overrides.gasLimit = gasLowerBound;
       }
